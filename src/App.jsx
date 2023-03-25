f964b72d54ee6437bf0bd00ec84c318d324e6dab
Gesiere Tarasele <taraselegesiere@gmail.com>
2023-03-25 12:16:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Card from './components/card'
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <div className="card-wrapper">
+        <Card />
+      </div>
+      <div class="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Gesiere Tarasele</a>.
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1> Simple, traffic-based pricing</h1>
+            <p>Sign-up for our 30-day trial. No credit card required.</p>
+        </header>
+    )
+}
+
+export default Header
