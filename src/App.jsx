d82b69e3a7201671ce529485f63351740aa78928
Gesiere Tarasele <taraselegesiere@gmail.com>
2023-03-25 12:23:24 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
-import Card from './components/card'
+import Card from './components/Card'
 
 function App() {
   return (
